c5c0e2d81963cdf49eaaf8f49d536e109a653575
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2023-03-12 00:21:27 +0200

    add functionality to edit employees

---
+++ client/src/components/Dashboard/TaskManager/EditEmployee/EditEmployee.jsx
@@ -0,0 +1,100 @@
+import './EditEmployee.scss';
+import { TextField, Button } from '@material-ui/core';
+import { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateField } from '@mui/x-date-pickers/DateField';
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+
+import { auth } from '../../../../utils/firebase';
+import * as apiService from '../../../../services/apiService';
+
+const EditEmployee = ({ employee, setEmployeeToEdit, setEmployeesToEdit }) => {
+    const [salary, setSalary] = useState(0);
+    const [open, setOpen] = useState(false);
+
+    const onEditEmployeeHandler = (e) => {
+        e.preventDefault();
+
+        if (salary > 0) {
+            apiService
+                .updateEmployee(employee.id, {
+                    ...employee,
+                    salary: salary,
+                    isNew: false,
+                })
+                .then(() => {
+                    setEmployeeToEdit(() => null);
+                    setEmployeesToEdit(() => []);
+                });
+        } else {
+            setOpen(true);
+        }
+    };
+
+    return (
+        <form id="new-employee-form">
+            <TextField className="textField" type="text" label="First name" variant="outlined" defaultValue={employee.firstName} disabled />
+            <TextField className="textField" type="text" label="Last name" variant="outlined" defaultValue={employee.lastName} disabled />
+            <br />
+            <TextField className="textField" type="text" label="Email" variant="outlined" defaultValue={employee.email} disabled />
+            <TextField className="textField" type="text" label="Phone number" variant="outlined" defaultValue={employee.phoneNumber} disabled />
+            <br />
+            <TextField className="textField" type="text" label="Date of birth" variant="outlined" defaultValue={employee.dateOfBirth} disabled />
+            <TextField
+                className="textField"
+                type="number"
+                label="Salary"
+                variant="outlined"
+                defaultValue={employee.salary}
+                onChange={(e) => setSalary(() => e.target.value)}
+            />
+            <br />
+            <div id='alert-ctr'>
+                <Box sx={{ width: '50%' }}>
+                    <Collapse in={open}>
+                        <Alert
+                            severity="error"
+                            action={
+                                <IconButton
+                                    aria-label="close"
+                                    color="inherit"
+                                    size="small"
+                                    onClick={() => {
+                                        setOpen(false);
+                                    }}
+                                >
+                                    X
+                                </IconButton>
+                            }
+                            sx={{ mb: 2 }}
+                        >
+                            {`You have to set salary first!`}
+                        </Alert>
+                    </Collapse>
+                </Box>
+            </div>
+            <br />
+            <Button
+                style={{
+                    marginTop: '10px',
+                    width: '200px',
+                    backgroundColor: '#404e67',
+                    boxShadow: '0px 10px 18px -11px white',
+                    letterSpacing: '2px',
+                }}
+                variant="contained"
+                color="primary"
+                onClick={onEditEmployeeHandler}
+            >
+                edit employee
+            </Button>
+        </form>
+    );
+};
+
+export default EditEmployee;
--- client/src/components/Dashboard/TaskManager/NewEmployees/NewEmployees.jsx
+++ client/src/components/Dashboard/TaskManager/NewEmployees/NewEmployees.jsx
@@ -5,13 +5,25 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import NewEmployees from '../NewEmployees/NewEmployees';
+import { useState } from 'react';
+import './NewEmployees.scss';
+import EditEmployee from '../EditEmployee/EditEmployee';
 
-const AllEmployees = ({ employees }) => {
-    return (
-        <div id="tasks-ctr">
-            <NewEmployees employees={employees?.filter(x => x.isNew === true)} />
 
+const NewEmployees = ({ employees }) => {
+    const [employeesToEdit, setEmployeesToEdit] = useState(employees);
+    const [employeeToEdit, setEmployeeToEdit] = useState();
+
+    const onEdintEmployeeInfoHandler = (employee) => {
+        setEmployeeToEdit(() => employee);
+    };
+
+    return employeesToEdit.length > 0 ? (
+        <>
+            {employeeToEdit && (
+                <EditEmployee employee={employeeToEdit} setEmployeeToEdit={setEmployeeToEdit} setEmployeesToEdit={setEmployeesToEdit} />
+            )}
+            <h2 className="table-title">New employees</h2>
             <TableContainer component={Paper}>
                 <Table aria-label="simple table" stickyHeader>
                     <TableHead>
@@ -21,13 +33,11 @@ const AllEmployees = ({ employees }) => {
                             <TableCell align="left">Email</TableCell>
                             <TableCell align="left">Phone Number</TableCell>
                             <TableCell align="left">Date of Birth</TableCell>
-                            <TableCell align="left">Salary</TableCell>
-                            <TableCell align="right">Completed Tasks</TableCell>
-                            <TableCell align="right">Uncompleted Tasks</TableCell>
+                            <TableCell align="left"></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {employees?.filter(x => x.isNew === false).map((employee, index) => (
+                        {employeesToEdit?.map((employee, index) => (
                             <TableRow key={employee.id}>
                                 <TableCell component="th" scope="row">
                                     {index + 1}
@@ -38,16 +48,18 @@ const AllEmployees = ({ employees }) => {
                                 <TableCell align="left">{employee.email}</TableCell>
                                 <TableCell align="left">{employee.phoneNumber}</TableCell>
                                 <TableCell align="left">{employee.dateOfBirth}</TableCell>
-                                <TableCell align="left">{employee.salary}</TableCell>
-                                <TableCell align="right">{employee.tasks.filter((t) => t.completed === true).length}</TableCell>
-                                <TableCell align="right">{employee.tasks.filter((t) => t.completed !== true).length}</TableCell>
+                                <TableCell align="left">
+                                    <i onClick={() => onEdintEmployeeInfoHandler(employee)} className="fas fa-edit"></i>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
-        </div>
+        </>
+    ) : (
+        ''
     );
 };
 
-export default AllEmployees;
+export default NewEmployees;
