f570b01f5248f625e7437bf73266c57a0fa0a2dc
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2023-03-13 02:57:53 +0200

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -13,39 +13,47 @@ import './App.css';
 
 function App() {
     const [userInfo, setUserInfo] = useState(null);
+    const [initialized, setInitialized] = useState(false);
 
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
             if (user) {
                 apiService.getOneEmployee(user._delegate.uid).then((data) => {
+                    setInitialized(true);
                     setUserInfo(() => data);
                 });
+            } else {
+                setInitialized(true);
             }
-        })
+        });
     }, []);
 
     const isTaskManager = userInfo?.role === 'taskManager';
 
     return (
         <div id="container">
-            <Header userEmail={userInfo?.email} isAuthenticated={!!userInfo} />
-            <Routes>
-                <Route
-                    index
-                    element={
-                        !userInfo ? (
-                            <Login />
-                        ) : isTaskManager ? (
-                            <TaskManagerDashboard />
-                        ) : (
-                            <EmployeeDashboard userInfo={userInfo} setUserInfo={setUserInfo} />
-                        )
-                    }
-                ></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/register" element={<Register />}></Route>
-                <Route path="/logout" element={<Logout setUserInfo={setUserInfo} />}></Route>
-            </Routes>
+            {initialized ? (
+                <>
+                    <Header userEmail={userInfo?.email} isAuthenticated={!!userInfo} />
+                    <Routes>
+                        <Route
+                            index
+                            element={
+                                !userInfo ? (
+                                    <Login />
+                                ) : isTaskManager ? (
+                                    <TaskManagerDashboard />
+                                ) : (
+                                    <EmployeeDashboard userInfo={userInfo} setUserInfo={setUserInfo} />
+                                )
+                            }
+                        ></Route>
+                        <Route path="/login" element={<Login />}></Route>
+                        <Route path="/register" element={<Register />}></Route>
+                        <Route path="/logout" element={<Logout setUserInfo={setUserInfo} />}></Route>
+                    </Routes>
+                </>
+            ) : null}
         </div>
     );
 }
